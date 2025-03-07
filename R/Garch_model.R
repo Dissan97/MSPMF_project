6e03079b194b6665b63a06237c943995c5ca88b8
dissan <dissanhamed@gmail.com>
2025-03-07 07:03:20 +0100

    [Univariate adjustament]

--- R/Garch_model.R
+++ R/Garch_model.R
@@ -1,7 +1,7 @@
 library(rugarch)
 
-get_garch_best_models <- function(returns, f_horizon) {
-    distributions <- c("std", "ged", "snorm", "sstd")
+get_garch_best_models <- function(returns, f_horizon, n_simulations = 1000) {
+    distributions <- c("std", "ged")#, "snorm", "sstd")
     
     best_shibata <- -Inf
     ml <- -Inf
@@ -11,14 +11,15 @@ get_garch_best_models <- function(returns, f_horizon) {
     best_dist <- ""
 
     for (dist in distributions) {
-        spec <- spec <- ugarchspec(
-            variance.model = list(model = "fGARCH", garchOrder = c(1,1), submodel = "GJRGARCH"),
+        spec <- ugarchspec(
+            variance.model = list(model = "fGARCH", garchOrder = c(1,1), submodel = "GARCH"),
             mean.model = list(armaOrder = c(1,1)),  
-            distribution.model = dist,
-            start.pars = list(omega = 0.01, alpha1 = 0.05, beta1 = 0.9, gamma1 = 0.01)  
+            distribution.model = dist
+            #start.pars = list(omega = 0.01, alpha1 = 0.05, beta1 = 0.9, gamma1 = 0.01)  
         )
         fit <- tryCatch(
-            ugarchfit(spec, returns, solver.control = list(maxeval = 20000)),
+            ugarchfit(spec, returns,# solver.control = list(maxeval = 20000),
+                     solver = "solnp"),
             error = function(e) NULL 
         )
         
@@ -35,7 +36,7 @@ get_garch_best_models <- function(returns, f_horizon) {
         shibata <- criteria[3]  
         lh <- likelihood(fit)
 
-        if (ml < lh) {
+        if (best_shibata < shibata) {
             ml <- lh
             best_shibata <- shibata
             best_dist <- dist
@@ -47,9 +48,26 @@ get_garch_best_models <- function(returns, f_horizon) {
         stop("Nessun modello valido trovato. Verifica i dati di input.")
     }
 
-    # Ottenere le previsioni
+    # volatility previsions
     forecast_garch <- ugarchforecast(best_model, n.ahead = f_horizon, method="bootstrap")
     params <- coef(best_model)
+                                 # **2. Simulazione bootstrap**
+    boot_results <- ugarchboot(best_model, n.ahead = f_horizon, method = "Partial", n.bootpred = n_simulations)
+    #show(boot_results)
+    # bootstrap
+    boot_forecasts_series <- boot_results@fseries
+    boot_forecasts_sigma <- boot_results@fsigma
+    #show(boot_forecasts)
+                             
+    lower_90 <- apply(boot_forecasts_series, 1, quantile, probs = 0.1, na.rm = TRUE)
+    upper_90 <- apply(boot_forecasts_series, 1, quantile, probs = 0.90, na.rm = TRUE)
+
+    lower_95 <- apply(boot_forecasts_series, 1, quantile, probs = 0.05, na.rm = TRUE)
+    upper_95 <- apply(boot_forecasts_series, 1, quantile, probs = 0.95, na.rm = TRUE)
+
+    lower_99 <- apply(boot_forecasts_series, 1, quantile, probs = 0.01, na.rm = TRUE)
+    upper_99 <- apply(boot_forecasts_series, 1, quantile, probs = 0.99, na.rm = TRUE)
+    
     #show(sigma(best_model))
     return (list(
         "distribution" = best_dist,
@@ -60,6 +78,15 @@ get_garch_best_models <- function(returns, f_horizon) {
         "conditional_mean" = fitted(forecast_garch),
         "omega" = if ("omega" %in% names(params)) params["omega"] else NA,
         "alpha1" = if ("alpha1" %in% names(params)) params["alpha1"] else NA,
-        "beta1" = if ("beta1" %in% names(params)) params["beta1"] else NA
+        "beta1" = if ("beta1" %in% names(params)) params["beta1"] else NA,
+        "boot_forecasts_series" = boot_forecasts_series,
+        "boot_forecasts_sigma" = boot_forecasts_sigma,
+        #**prevision band bootstrap**
+        "upper_90" = upper_90,
+        "lower_90" = lower_90,
+        "upper_95" = upper_95,
+        "lower_95" = lower_95,
+        "upper_99" = upper_99,
+        "lower_99" = lower_99
     ))
 }
--- R/RMGarch_model.R
+++ R/RMGarch_model.R
@@ -1,26 +1,27 @@
 library(rugarch)
 library(rmgarch)
 
-get_best_dcc_garch <- function(r_rets, n_days = 5, model_type = "GJRGARCH", dist_type = "std") {
+dcc_garch_fit <- function(r_rets, n_days = 5, model_type = "GARCH", dist_type = "ged") {
     
     if (!is.matrix(r_rets)) r_rets <- as.matrix(r_rets)
     n <- dim(r_rets)[2]
-    if (n < 2) stop("Errore: DCC-GARCH richiede almeno due serie temporali.")  
-    if (any(is.na(r_rets))) stop("Errore: Il dataset contiene NA.")
+    if (n < 2) stop("Errore: DCC-GARCH at least 2 timeseries.")  
+    if (any(is.na(r_rets))) stop("Errore: Il dataset has NA.")
     
     univariate_spec <- ugarchspec(
-        mean.model = list(armaOrder = c(0,0)),
+        mean.model = list(armaOrder = c(1,1)),
         variance.model = list(model = "fGARCH", garchOrder = c(1,1), submodel = model_type),
         distribution.model = dist_type
     )
 
     dcc_spec <- dccspec(
         uspec = multispec(replicate(n, univariate_spec)),
-        dccOrder = c(1,1),
-        distribution = "mvlaplace"
+        dccOrder = c(1, 1),
+        distribution = "mvlaplace",
+        lag.max = 10
     )
 
-    # Stima del modello con gestione degli errori
+    
     dcc_fit <- tryCatch({
         dccfit(dcc_spec, data = r_rets)
     }, error = function(e) {
@@ -29,87 +30,34 @@ get_best_dcc_garch <- function(r_rets, n_days = 5, model_type = "GJRGARCH", dist
     })
 
     if (is.null(dcc_fit)) return(NULL)
-
-    # Previsione della matrice di covarianza
+    
     forecasts <- tryCatch({
         dccforecast(dcc_fit, n.ahead = n_days)
     }, error = function(e) {
         message("Prevision error: ", e$message)
         return(NULL)
     })
+    residuals_matrix <- residuals(dcc_fit)
+    
+    
 
     if (is.null(forecasts)) return(NULL)
-
+    #show(dccforecast(dcc_fit, n.ahead = n_days))
+    write.csv(residuals_matrix, "./temp/residual_matrix.csv")
+    write.csv(rcov(dcc_fit), "./temp/rcov.csv")
+    write.csv(rcor(dcc_fit), "./temp/rcor.csv")
+    write.csv(rskew(dcc_fit), "./temp/rskew.csv")
+    write.csv(sigma(dcc_fit), "./temp/sigma.csv")
+    write.csv(coef(dcc_fit), "./temp/coef.csv")
+    write.csv(residuals_matrix / sigma(dcc_fit), "./temp/std_residuals.csv")
+    
     # Output strutturato
     return(list(
         fit = dcc_fit,
         forecasts = forecasts@mforecast$H,
-        correlations = rcov(dcc_fit)  
+        correlations = rcov(dcc_fit),
+        residuals =as.matrix(residuals_matrix)
     ))
 }
 
 
-get_best_go_garch <- function(list_returns, f_horizon) {
-    
-}
-
-get_best_cdcc_garch <- function(r_rets, n_days = 5, model_type = "GJRGARCH", dist_type = "sstd") {
-    
-    # Controllo sugli input
-    if (!is.matrix(r_rets)) r_rets <- as.matrix(r_rets)
-    n <- dim(r_rets)[2]
-    if (n < 2) stop("Errore: cDCC-GARCH richiede almeno due serie temporali.")  
-    if (any(is.na(r_rets))) stop("Errore: Il dataset contiene NA.")
-    
-    message("🟢 Input data check: OK")
-    
-    # Specifica GARCH univariato flessibile
-    univariate_spec <- ugarchspec(
-        mean.model = list(armaOrder = c(0,0)),
-        variance.model = list(model = "fGARCH", garchOrder = c(1,1), submodel = model_type),
-        distribution.model = dist_type
-    )
-
-    # Specifica cDCC-GARCH flessibile
-    cdcc_spec <- dccspec(
-        uspec = multispec(replicate(n, univariate_spec)),
-        dccOrder = c(1,1),
-        lag.criterion = "AIC",
-        robust.control = list("gamma" = 0.25, "delta" = 0.01, "nc" = 10, "ns" = 500),
-        model = "aDCC",
-        distribution = "mvlaplace"
-    )
-
-    message("🟢 DCC specification: OK")
-
-    # Stima del modello con gestione errori
-    cdcc_fit <- tryCatch({
-        fit <- dccfit(cdcc_spec, data = r_rets)
-        message("🟢 Model fitting: OK")
-        return(fit)
-    }, error = function(e) {
-        message("❌ Errore nella stima del cDCC-GARCH: ", e$message)
-        return(NULL)
-    })
-
-    if (is.null(cdcc_fit)) return(NULL)
-
-    # Previsione della matrice di covarianza
-    forecasts <- tryCatch({
-        forecast <- dccforecast(cdcc_fit, n.ahead = n_days)
-        message("🟢 Forecasting: OK")
-        return(forecast)
-    }, error = function(e) {
-        message("❌ Errore nella previsione cDCC-GARCH: ", e$message)
-        return(NULL)
-    })
-
-    if (is.null(forecasts)) return(NULL)
-
-    # Output strutturato
-    return(list(
-        fit = cdcc_fit,
-        forecasts = forecasts@mforecast$H,
-        correlations = rcov(cdcc_fit)
-    ))
-}
